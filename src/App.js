462360e8a03fdb9f4fe0103083a2a79a6b196424
Evgeny Korolev <mts9832899777@gmail.com>
2022-06-02 17:25:28 +0700

    create a repository

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className='app-wrapper'>
+      <header className='header'>
+        <img src={require('./logo192.png')} />
+      </header>
+      <nav className='nav'>
+        <div><a href='#'>Profile</a></div>
+        <div><a href='#'>Messages</a></div>
+        <div><a href='#'>News</a></div>
+        <div><a href='#'>Music</a></div>
+        <div><a href='#'>Settings</a></div>
+      </nav>
+      <div className='content'>
+        <img src={require('./profilePhoto.png')} />
+        <div>
+        ava + description
+      </div>
+      <div>
+        My posts
+        <div>
+          New post
+        </div>
+        <div>
+          <div>Post 1</div>
+          <div>Post 2</div>
+        </div>
+      </div>
+      </div>
+     
+    </div>);}
+
+export default App;
